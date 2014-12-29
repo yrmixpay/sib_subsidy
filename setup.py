0b8c7ab94aa3ccd1133b86e921b5e2faaa84c9a4
vertoe <vertoe@qhor.net>
2014-12-30 00:41:27 +0100

    Fix subsidy for testnet finally.

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from distutils.core import setup, Extension
 darkcoin_module = Extension('darkcoin_subsidy', sources = ['darkcoin_subsidy.cpp'])
 
 setup (name = 'darkcoin_subsidy',
-       version = '1.0',
-       description = 'Subsidy function for DarkCoin',
+       version = '1.1',
+       description = 'Subsidy function for Darkcoin',
        ext_modules = [darkcoin_module])
