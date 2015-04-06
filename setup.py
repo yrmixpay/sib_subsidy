eeb535fa033303c5c3d671199dfd3eb7e84aaacf
Jake Haas <jake@jakehaas.com>
2015-04-06 02:57:49 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from distutils.core import setup, Extension
 dash_subsidy_module = Extension('dash_subsidy', sources = ['dash_subsidy.cpp'])
 
 setup (name = 'dash_subsidy',
-       version = '1.2',
+       version = '1.3',
        description = 'Subsidy function for Dash',
        ext_modules = [dash_subsidy_module])
